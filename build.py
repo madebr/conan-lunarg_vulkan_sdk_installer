2fad9a9efde2efebca33d2f714573dd1dfe36cbb
Anonymous Maarten <anonymous.maarten@gmail.com>
2019-02-07 18:45:04 +0100

    se bincrafters-package-tools

--- build.py
+++ build.py
@@ -1,9 +1,12 @@
-from conan.packager import ConanMultiPackager
+# -*- coding: utf-8 -*-
+
+from bincrafters import build_shared
+
 import os
 import platform
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    builder = build_shared.get_builder()
     os.environ['LUNARG_HUMAN'] = '1'
     if platform.system() == 'Windows':
         builder.add(settings={'arch_build': 'x86'})
